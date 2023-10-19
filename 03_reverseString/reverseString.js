c97b50a392c611ff3aa5a0bbed6d84a38d2a27a1
Code-Dub <66441118+code-dub@users.noreply.github.com>
2023-10-19 10:43:49 +0530

    exercises for repeat string and reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,25 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
-};
+    let stringArray = word.split("");
+    // let stringResult =
+    console.log(stringArray);
+    let returnArray = [];
+    console.log(returnArray);
+
+    for (let i = 0; i < (stringArray.length); i++){
+        returnArray[i] = stringArray[(stringArray.length - 1 - i)];
+        // return returnArray;
+    }
+
+    return returnArray.join('');
+
+
+        
+    };
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
