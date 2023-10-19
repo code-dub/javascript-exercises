6629aaf3bec846cf12a0f2714236d7a13fa8ae5d
Code-Dub <66441118+code-dub@users.noreply.github.com>
2023-10-19 15:29:54 +0530

    add temperature conversion functions to and from celcius/ farheneit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhenheit) {
+
+  const celsius = (((farhenheit-32) * 5) / 9);
+  return Math.round(celsius * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  const farhenheit = ((celsius * 9/5) + 32)
+  return Math.round(farhenheit * 10)/ 10;
 };
+
+convertToCelsius(97.8);
+
 
 // Do not edit below this line
 module.exports = {
